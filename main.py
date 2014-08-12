785b12f4ede13d63d7ab68ada257879fac18a157
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2014-08-11 17:57:01 -0700

    Stable Start

--- main.py
+++ main.py
@@ -1,134 +1,381 @@
-from google.appengine.ext import ndb
-from google.appengine.api import users
-
-import os
-import json
-import jinja2
+#!/usr/bin/env python
 import logging
+import jinja2
+import os
 import webapp2
-import urllib2
-import datetime
-import time
+import urllib, hashlib
 
-class Student(ndb.Model):
-	name = ndb.StringProperty(required=True)
-	uni = ndb.StringProperty()
-	birthday = ndb.DateProperty()
+from google.appengine.ext import ndb
 
-	def url(self):
-		return "/profile?ID=%s" % self.key.id()
+from webapp2_extras import auth
+from webapp2_extras import sessions
 
-	# Bulk Deletions Later
-	def delete(self):
-		self.key.delete()
+from webapp2_extras.auth import InvalidAuthIdError
+from webapp2_extras.auth import InvalidPasswordError
+
+from models import User, Lab
 
 jinja_environment = jinja2.Environment(loader=
-	jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-# Bootstrap and Parsely
-header = '<head>	<link rel="stylesheet" type="text/css" href="/s/b/css/bootstrap.css">	<script src="/s/jquery-git1.js"></script>	<script src="/s/b/js/bootstrap.js"></script>	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">	<link rel="stylesheet" type="text/css" href="/s/parsley.css">	<script src="/s/parsley.js"></script>	</head>'
+def user_required(handler):
+  """
+    Decorator that checks if there's a user associated with the current session.
+    Will also fail if there's no session present.
+  """
+  def check_login(self, *args, **kwargs):
+    auth = self.auth
+    if not auth.get_user_by_session():
+      self.redirect(self.uri_for('login'), abort=True)
+    else:
+      return handler(self, *args, **kwargs)
 
-def SetTemplate(pageName):
-	return jinja_environment.get_template("%s.html" % pageName)
+  return check_login
 
-class DashboardHandler(webapp2.RequestHandler):
-	def get(self):
-		self.response.out.write(header)
 
-		pageName = "dashboard"
-		template = SetTemplate(pageName)
+class BaseHandler(webapp2.RequestHandler):
+  @webapp2.cached_property
+  def auth(self):
+    """Shortcut to access the auth instance as a property."""
+    return auth.get_auth()
 
-		results = Student.query().fetch()
-		template_values = { "students" : results }
+  @webapp2.cached_property
+  def user_info(self):
+    """Shortcut to access a subset of the user attributes that are stored
+    in the session.
 
-		self.response.out.write(template.render(template_values))
+    The list of attributes to store in the session is specified in
+      config['webapp2_extras.auth']['user_attributes'].
+    :returns
+      A dictionary with most user information
+    """
+    return self.auth.get_user_by_session()
 
-class ProfileHandler(webapp2.RequestHandler):
-	def get(self):
-		self.response.out.write(header)
+  @webapp2.cached_property
+  def user(self):
+    """Shortcut to access the current logged in user.
 
-		pageName = "student"
-		template = SetTemplate(pageName)
+    Unlike user_info, it fetches information from the persistence layer and
+    returns an instance of the underlying model.
 
-		if self.request.get("new") == "true":
-			name = str(self.request.get("name"))
-			uni = self.request.get("uni")
-			birthday = str(self.request.get("birthday"))
-			
-			if birthday == "":
-				birthday = datetime.datetime.strptime("1900-1-1", "%Y-%m-%d")
-			else:
-				birthday = datetime.datetime.strptime(birthday, "%Y-%m-%d")
+    :returns
+      The instance of the user model associated to the logged in user.
+    """
+    u = self.user_info
+    return self.user_model.get_by_id(u['user_id']) if u else None
 
-			student = Student(name=name, uni=uni, birthday=birthday)
-			student.put()
-		else:
-			ID = int(self.request.get("ID"))
-			student = Student.get_by_id(ID)
-			name = student.name
-			uni = student.uni
-			birthday = student.birthday
+  @webapp2.cached_property
+  def user_model(self):
+    """Returns the implementation of the user model.
 
-		template_values = {
-			"student" :
-				{
-					"name" : name,
-					"uni" : uni,
-					"birthday" : birthday,
-				}
-		}
+    It is consistent with config['webapp2_extras.auth']['user_model'], if set.
+    """    
+    return self.auth.store.user_model
 
-		self.response.out.write(template.render(template_values))
+  @webapp2.cached_property
+  def session(self):
+      """Shortcut to access the current session."""
+      return self.session_store.get_session(backend="datastore")
 
-# Start Generic Handlers
+  def render_template(self, view_filename, params=None):
+    if not params:
+      params = {}
+    user = self.user
+    params['user'] = user
+    template = jinja_environment.get_template('views/%s' % view_filename)
+    self.response.out.write(template.render(params))
 
-class GenericHandler(webapp2.RequestHandler):
-	def __init__(self, request, response):
-		self.initialize(request, response)
-		self.get()
-		self.response.out.write(header)
-		template = SetTemplate(self.pageName)
-		self.response.out.write(template.render())
+  def display_message(self, message):
+    """Utility function to display a template with a simple message."""
+    params = {
+      'message': message
+    }
+    self.render_template('message.html', params)
 
-class HomeHandler(GenericHandler):
-	def get(self):
-		self.pageName = "home"
+  # this is needed for webapp2 sessions to work
+  def dispatch(self):
+      # Get a session store for this request.
+      self.session_store = sessions.get_store(request=self.request)
 
-class SingupHandler(GenericHandler):
-	def get(self):
-		self.pageName = "signup"
+      try:
+          # Dispatch the request.
+          webapp2.RequestHandler.dispatch(self)
+      finally:
+          # Save all sessions.
+          self.session_store.save_sessions(self.response)
 
-class NotFoundHandler(GenericHandler):
-	def get(self):
-		self.pageName = "404"
+class MainHandler(BaseHandler):
+  def get(self):
+	if self.user:
+		self.redirect(self.user.profile_link())
+	else:
+		self.render_template('home.html')
 
-class WorkspaceHandler(GenericHandler):
-	def get(self):
-		self.pageName = "workspace"
+class SignupHandler(BaseHandler):
+  def get(self):
+    self.render_template('signup.html')
 
-# End Generic Handlers
+  def post(self):
+    email = self.request.get('email')
+    name = self.request.get('name')
+    password = self.request.get('password')
+    last_name = self.request.get('lastname')
 
-def GetAnswers(inputValues):
-	answers = []
-	guesses = inputValues.keys()
-	correct = [ "7", "Sacramento"]
-	questions = [ "What is 3 + 4?", "What is the capital of California?" ]
+    unique_properties = ['email_address']
+    user_data = self.user_model.create_user(email,
+      unique_properties, email_address=email, name=name, password_raw=password,
+      last_name=last_name, verified=False)
+    if not user_data[0]: #user_data is a tuple
+      self.display_message('Unable to create user for email %s because of \
+        duplicate keys %s' % (email_address, user_data[1]))
+      return
+    
+    user = user_data[1]
+    user_id = user.get_id()
 
-	for index in xrange(0, len(inputValues)):
-		answers.append({
-		"guess" : guesses[index],
-		"correct" : correct[index],
-		"questions" : questions[index],
-		})
+    token = self.user_model.create_signup_token(user_id)
 
-	return answers
+    verification_url = self.uri_for('verification', type='v', user_id=user_id,
+      signup_token=token, _full=True)
+
+    msg = 'Send an email to user in order to verify their address. \
+          They will be able to do so by visiting <a href="{url}">{url}</a>'
+
+    self.display_message(msg.format(url=verification_url))
+
+class ForgotPasswordHandler(BaseHandler):
+  def get(self):
+    self._serve_page()
+
+  def post(self):
+    email = self.request.get('email')
+
+    user = self.user_model.get_by_auth_id(email)
+    if not user:
+      logging.info('Could not find any user entry for email %s', email)
+      self._serve_page(not_found=True)
+      return
+
+    user_id = user.get_id()
+    token = self.user_model.create_signup_token(user_id)
+
+    verification_url = self.uri_for('verification', type='p', user_id=user_id,
+      signup_token=token, _full=True)
+
+    msg = 'Send an email to user in order to reset their password. \
+          They will be able to do so by visiting <a href="{url}">{url}</a>'
+
+    self.display_message(msg.format(url=verification_url))
+  
+  def _serve_page(self, not_found=False):
+    email = self.request.get('email')
+    params = {
+      'email': email,
+      'not_found': not_found
+    }
+    self.render_template('forgot.html', params)
+
+
+class VerificationHandler(BaseHandler):
+  def get(self, *args, **kwargs):
+    user = None
+    user_id = kwargs['user_id']
+    signup_token = kwargs['signup_token']
+    verification_type = kwargs['type']
+
+    # it should be something more concise like
+    # self.auth.get_user_by_token(user_id, signup_token)
+    # unfortunately the auth interface does not (yet) allow to manipulate
+    # signup tokens concisely
+    user, ts = self.user_model.get_by_auth_token(int(user_id), signup_token,
+      'signup')
+
+    if not user:
+      logging.info('Could not find any user with id "%s" signup token "%s"',
+        user_id, signup_token)
+      self.abort(404)
+    
+    # store user data in the session
+    self.auth.set_session(self.auth.store.user_to_dict(user), remember=True)
+
+    if verification_type == 'v':
+      # remove signup token, we don't want users to come back with an old link
+      self.user_model.delete_signup_token(user.get_id(), signup_token)
+
+      if not user.verified:
+        user.verified = True
+        user.put()
+
+      self.display_message('User email address has been verified.')
+      return
+    elif verification_type == 'p':
+      # supply user to the page
+      params = {
+        'user': user,
+        'token': signup_token
+      }
+      self.render_template('resetpassword.html', params)
+    else:
+      logging.info('verification type not supported')
+      self.abort(404)
+
+class SetPasswordHandler(BaseHandler):
+
+  @user_required
+  def post(self):
+    password = self.request.get('password')
+    old_token = self.request.get('t')
+
+    if not password or password != self.request.get('confirm_password'):
+      self.display_message('passwords do not match')
+      return
+
+    user = self.user
+    user.set_password(password)
+    user.put()
+
+    # remove signup token, we don't want users to come back with an old link
+    self.user_model.delete_signup_token(user.get_id(), old_token)
+    
+    self.display_message('Password updated')
+
+class LoginHandler(BaseHandler):
+  def get(self):
+    self._serve_page()
+
+  def post(self):
+    email = self.request.get('email')
+    password = self.request.get('password')
+    try:
+      u = self.auth.get_user_by_password(email, password, remember=True,
+        save_session=True)
+      self.redirect(self.uri_for('home'))
+    except (InvalidAuthIdError, InvalidPasswordError) as e:
+      logging.info('Login failed for user %s because of %s', email, type(e))
+      self._serve_page(True)
+
+  def _serve_page(self, failed=False):
+    email = self.request.get('email')
+    params = {
+      'email': email,
+      'failed': failed
+    }
+    self.render_template('login.html', params)
+
+class LogoutHandler(BaseHandler):
+  def get(self):
+    self.auth.unset_session()
+    self.redirect(self.uri_for('home'))
+
+class AuthenticatedHandler(BaseHandler):
+  @user_required
+  def get(self):
+    self.render_template('authenticated.html')
+
+class ProfileHandler(BaseHandler):
+  @user_required
+  def get(self, *args, **kwargs):
+
+  	user_id = int(kwargs['user_id'])
+  	name = kwargs['name'].lower()
+  	request_type = kwargs['type'].lower()
+  	last_name = kwargs['last_name'].lower()
+
+   	local_user = User.get_by_id(user_id)
+   	user = self.user
+   	if request_type == 'p':
+	  	if local_user and local_user.name.lower() == name and local_user.key.id() == user_id:
+	  		query = Lab.query()
+	  		if local_user.key.id() == user.key.id():
+	  			labs = query.filter( Lab.collaborators.IN([local_user.email_address])).fetch()
+	  		else:
+	  			labs = query.filter( Lab.collaborators.IN([local_user.email_address]) and  Lab.private == False).fetch()
+	  		params = {
+	  		'labs': labs,
+	  		'local_user': local_user
+	  		}
+	  		self.render_template('profile.html', params)
+	  	else:
+	  		self.display_message('The user who\'s profile you attempted to view does not exist. <a href="/p/{0}.{1}/{2}">Go your profile.</a>'.format(user.name, user.last_name, user.key.id()))
+	else:
+		self.redirect(self.uri_for('home'))
+
+class NewLabHandler(BaseHandler):
+  @user_required
+  def get(self):
+  	self.render_template('new_lab.html')
+  def post(self):
+  	name = self.request.get('name')
+  	owner = self.request.get('owner')
+	collaborators = self.request.get('emails') + ',' + owner
+	private = self.request.get('private')
+	if private.lower() == 'true':
+		private = True
+	else:
+		private = False
+	
+	lab = Lab(name=name,
+              owner=owner,
+              private=private,
+              collaborators= collaborators.split(","))
+	lab.put()
+	self.redirect(self.uri_for('home'))
+
+class LabHandler(BaseHandler):
+  @user_required
+  def get(self, *args, **kwargs):
+    lab_id = kwargs['lab_id']
+    lab = Lab.get_by_id(int(lab_id))
+    if lab:
+    	params = {
+    	'lab': lab
+    	}
+    	lab.put()
+    	self.render_template('workspace.html', params)
+    else:
+    	params = {
+    	'lab_id': lab_id
+    	}
+    	self.display_message('There is no such lab registered under your name. <a href="/new_lab">Create A New Lab</a>')
+
+class DeleteLabHandler(webapp2.RequestHandler):
+  def get(self):
+    lab_id = int(self.request.get('id'))
+    lab = Lab.get_by_id(lab_id)
+    if lab:
+      lab.key.delete()
+      self.redirect(self.uri_for('home'))
+    else:
+     self.display_message('There is no lab by this id.')
+
+config = {
+  'webapp2_extras.auth': {
+    'user_model': 'models.User',
+    'user_attributes': ['name']
+  },
+  'webapp2_extras.sessions': {
+    'secret_key': '{z-0NJ]?VmFZTWvHX{;jGzO<c4-@Uk58 b|Ak }_wu+1mWk )>Vc5K7{b--fj%%l'
+  }
+}
 
 routes = [
-		("/profile", ProfileHandler),
-		("/dashboard", DashboardHandler),
-		("/signup", SingupHandler),
-		("/", WorkspaceHandler),
-		("/.*", NotFoundHandler),
+    webapp2.Route('/', MainHandler, name='home'),
+    webapp2.Route('/signup', SignupHandler),
+    webapp2.Route('/<type:v|p>/<user_id:\d+>-<signup_token:.+>',
+      handler=VerificationHandler, name='verification'),
+    webapp2.Route('/<type:l|p>/<lab_id:\d+>',
+      handler=LabHandler, name='lab'),
+    webapp2.Route('/new_lab',
+      handler=NewLabHandler, name='newlab'),
+    webapp2.Route('/<type:u|p>/<name:.+>.<last_name:.+>/<user_id:\d+>',
+      handler=ProfileHandler, name='profile'),
+    webapp2.Route('/password', SetPasswordHandler),
+    webapp2.Route('/login', LoginHandler, name='login'),
+    webapp2.Route('/logout', LogoutHandler, name='logout'),
+    webapp2.Route('/forgot', ForgotPasswordHandler, name='forgot'),
+    webapp2.Route('/authenticated', AuthenticatedHandler, name='authenticated')
 ]
 
-app = webapp2.WSGIApplication(routes, debug=True)
+app = webapp2.WSGIApplication(routes, debug=True, config=config)
+
+logging.getLogger().setLevel(logging.DEBUG)
---
+++ models.py
@@ -0,0 +1,74 @@
+import time
+import webapp2_extras.appengine.auth.models
+import urllib, hashlib
+
+from google.appengine.ext import ndb
+
+from webapp2_extras import security
+
+class User(webapp2_extras.appengine.auth.models.User):
+  def set_password(self, raw_password):
+    """Sets the password for the current user
+
+    :param raw_password:
+        The raw password which will be hashed and stored
+    """
+    self.password = security.generate_password_hash(raw_password, length=12)
+  def profile_link(self):
+    return "/p/{0}.{1}/{2}".format(self.name, self.last_name, self.key.id())
+  def gravatarize(self):
+    default = ""
+    gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(self.email_address.lower()).hexdigest() + "?"
+    gravatar_url += urllib.urlencode({'d':default, 's':str(200)})
+    return gravatar_url
+
+  @classmethod
+  def get_by_auth_token(cls, user_id, token, subject='auth'):
+    """Returns a user object based on a user ID and token.
+
+    :param user_id:
+        The user_id of the requesting user.
+    :param token:
+        The token string to be verified.
+    :returns:
+        A tuple ``(User, timestamp)``, with a user object and
+        the token timestamp, or ``(None, None)`` if both were not found.
+    """
+    token_key = cls.token_model.get_key(user_id, subject, token)
+    user_key = ndb.Key(cls, user_id)
+    # Use get_multi() to save a RPC call.
+    valid_token, user = ndb.get_multi([token_key, user_key])
+    if valid_token and user:
+        timestamp = int(time.mktime(valid_token.created.timetuple()))
+        return user, timestamp
+
+    return None, None
+
+"""
+
+    check = True
+    i = 0
+    while check:
+        i += 1
+        user = self.user_model.get_by_auth_id(username)
+        if not user:
+            check = False
+        else:
+            check = True
+            username += '.' + str(i)
+    user_name = username
+"""
+
+
+
+class Lab(ndb.Model):
+  name = ndb.StringProperty(required=True)
+  collaborators = ndb.StringProperty(repeated=True)
+  owner = ndb.StringProperty(required=True)
+  private = ndb.BooleanProperty()
+  def destroy_url(self):
+    return '/l/delete?id=%s' % self.key.id()
+  
+  def lab_link(self):
+    return "/l/{0}".format(self.key.id())
+
