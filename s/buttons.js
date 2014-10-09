75bca8b1564bd675314b89d78055353a28255c8f
wilfriedE <wilfried.hounyo@gmail.com>
2014-10-08 23:22:52 -0400

    First Template for OpenCo-LABS

---
+++ s/buttons.js
@@ -0,0 +1,29 @@
+{
+  "buttons": [
+    {
+      "name": "Browse",
+      "btnName": "browser",
+      "description": "An iframe based browser"
+    },
+    {
+      "name": "Search",
+      "btnName": "gcsearch",
+      "description": "Google Custom Search"
+    },
+    {
+      "name": "Youtube",
+      "btnName": "youtube",
+      "description": ""
+    },
+    {
+      "name": "Spotify",
+      "btnName": "spotify",
+      "description": ""
+    },
+    {
+      "name": "<Your Button Name>",
+      "btnName": "<Your File Name>",
+      "description": "<your Button Description>"
+    }
+  ]
+}
